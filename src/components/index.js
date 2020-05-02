7e6968f1ae48bc5cef607fe126d88b112a261207
hardyyy <hardik.kuwar@techmatters.com>
2020-05-02 20:16:49 +0530

    complete covid-19 tracker from scratch

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Card from './Cards/Cards';
+import Chart from './Chartjs/Chart';
+import CountryPicker from './CountryPicker/CountryPicker'
+
+export {
+    Card,
+    Chart,
+    CountryPicker
+}
